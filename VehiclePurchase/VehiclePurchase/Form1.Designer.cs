fbcde02c6bcfc62d2a56659bcf07db098acb9abb
Sam-Greenlee <124869389+Sam-Greenlee@users.noreply.github.com>
2023-02-10 13:37:05 -0600

    Add files via upload

---
+++ VehiclePurchase/VehiclePurchase/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace VehiclePurchase
+{
+    partial class frmCenterOfCars
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkGPSNavigation = new System.Windows.Forms.CheckBox();
+            this.chkLeatherInterior = new System.Windows.Forms.CheckBox();
+            this.chkStereoSystem = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdoCustomizedDetailing = new System.Windows.Forms.RadioButton();
+            this.rdoPearl = new System.Windows.Forms.RadioButton();
+            this.rdoStandard = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtSalePrice = new System.Windows.Forms.TextBox();
+            this.txtAAndFinish = new System.Windows.Forms.TextBox();
+            this.txtSubtotal = new System.Windows.Forms.TextBox();
+            this.txtSalesTax = new System.Windows.Forms.TextBox();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.txtTradeInAllowance = new System.Windows.Forms.TextBox();
+            this.txtAmountDue = new System.Windows.Forms.TextBox();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkGPSNavigation);
+            this.groupBox1.Controls.Add(this.chkLeatherInterior);
+            this.groupBox1.Controls.Add(this.chkStereoSystem);
+            this.groupBox1.Location = new System.Drawing.Point(37, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 160);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Accessories";
+            // 
+            // chkGPSNavigation
+            // 
+            this.chkGPSNavigation.AutoSize = true;
+            this.chkGPSNavigation.Location = new System.Drawing.Point(20, 105);
+            this.chkGPSNavigation.Name = "chkGPSNavigation";
+            this.chkGPSNavigation.Size = new System.Drawing.Size(130, 21);
+            this.chkGPSNavigation.TabIndex = 2;
+            this.chkGPSNavigation.Text = "GPS Navigation";
+            this.chkGPSNavigation.UseVisualStyleBackColor = true;
+            // 
+            // chkLeatherInterior
+            // 
+            this.chkLeatherInterior.AutoSize = true;
+            this.chkLeatherInterior.Location = new System.Drawing.Point(20, 61);
+            this.chkLeatherInterior.Name = "chkLeatherInterior";
+            this.chkLeatherInterior.Size = new System.Drawing.Size(127, 21);
+            this.chkLeatherInterior.TabIndex = 1;
+            this.chkLeatherInterior.Text = "Leather Interior";
+            this.chkLeatherInterior.UseVisualStyleBackColor = true;
+            // 
+            // chkStereoSystem
+            // 
+            this.chkStereoSystem.AutoSize = true;
+            this.chkStereoSystem.Location = new System.Drawing.Point(20, 21);
+            this.chkStereoSystem.Name = "chkStereoSystem";
+            this.chkStereoSystem.Size = new System.Drawing.Size(122, 21);
+            this.chkStereoSystem.TabIndex = 0;
+            this.chkStereoSystem.Text = "Stereo System";
+            this.chkStereoSystem.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdoCustomizedDetailing);
+            this.groupBox2.Controls.Add(this.rdoPearl);
+            this.groupBox2.Controls.Add(this.rdoStandard);
+            this.groupBox2.Location = new System.Drawing.Point(37, 279);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(235, 159);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Exterior Finish";
+            // 
+            // rdoCustomizedDetailing
+            // 
+            this.rdoCustomizedDetailing.AutoSize = true;
+            this.rdoCustomizedDetailing.Location = new System.Drawing.Point(20, 122);
+            this.rdoCustomizedDetailing.Name = "rdoCustomizedDetailing";
+            this.rdoCustomizedDetailing.Size = new System.Drawing.Size(161, 21);
+            this.rdoCustomizedDetailing.TabIndex = 2;
+            this.rdoCustomizedDetailing.TabStop = true;
+            this.rdoCustomizedDetailing.Text = "Customized Detailing";
+            this.rdoCustomizedDetailing.UseVisualStyleBackColor = true;
+            // 
+            // rdoPearl
+            // 
+            this.rdoPearl.AutoSize = true;
+            this.rdoPearl.Location = new System.Drawing.Point(20, 82);
+            this.rdoPearl.Name = "rdoPearl";
+            this.rdoPearl.Size = new System.Drawing.Size(62, 21);
+            this.rdoPearl.TabIndex = 1;
+            this.rdoPearl.TabStop = true;
+            this.rdoPearl.Text = "Pearl";
+            this.rdoPearl.UseVisualStyleBackColor = true;
+            // 
+            // rdoStandard
+            // 
+            this.rdoStandard.AutoSize = true;
+            this.rdoStandard.Location = new System.Drawing.Point(20, 40);
+            this.rdoStandard.Name = "rdoStandard";
+            this.rdoStandard.Size = new System.Drawing.Size(87, 21);
+            this.rdoStandard.TabIndex = 0;
+            this.rdoStandard.TabStop = true;
+            this.rdoStandard.Text = "Standard";
+            this.rdoStandard.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(357, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sale Price:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(276, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Accessories and Finish:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(369, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Subtotal:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(329, 231);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sales Tax (8%)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(389, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Total:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(300, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 17);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Trade-In Allowance:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(343, 404);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Amount Due:";
+            // 
+            // txtSalePrice
+            // 
+            this.txtSalePrice.Location = new System.Drawing.Point(439, 82);
+            this.txtSalePrice.Name = "txtSalePrice";
+            this.txtSalePrice.Size = new System.Drawing.Size(100, 22);
+            this.txtSalePrice.TabIndex = 3;
+            // 
+            // txtAAndFinish
+            // 
+            this.txtAAndFinish.Location = new System.Drawing.Point(439, 118);
+            this.txtAAndFinish.Name = "txtAAndFinish";
+            this.txtAAndFinish.ReadOnly = true;
+            this.txtAAndFinish.Size = new System.Drawing.Size(100, 22);
+            this.txtAAndFinish.TabIndex = 4;
+            // 
+            // txtSubtotal
+            // 
+            this.txtSubtotal.Location = new System.Drawing.Point(439, 187);
+            this.txtSubtotal.Name = "txtSubtotal";
+            this.txtSubtotal.ReadOnly = true;
+            this.txtSubtotal.Size = new System.Drawing.Size(100, 22);
+            this.txtSubtotal.TabIndex = 5;
+            // 
+            // txtSalesTax
+            // 
+            this.txtSalesTax.Location = new System.Drawing.Point(439, 231);
+            this.txtSalesTax.Name = "txtSalesTax";
+            this.txtSalesTax.ReadOnly = true;
+            this.txtSalesTax.Size = new System.Drawing.Size(100, 22);
+            this.txtSalesTax.TabIndex = 6;
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(439, 313);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtTotal.TabIndex = 7;
+            // 
+            // txtTradeInAllowance
+            // 
+            this.txtTradeInAllowance.Location = new System.Drawing.Point(439, 360);
+            this.txtTradeInAllowance.Name = "txtTradeInAllowance";
+            this.txtTradeInAllowance.Size = new System.Drawing.Size(100, 22);
+            this.txtTradeInAllowance.TabIndex = 8;
+            // 
+            // txtAmountDue
+            // 
+            this.txtAmountDue.Location = new System.Drawing.Point(439, 401);
+            this.txtAmountDue.Name = "txtAmountDue";
+            this.txtAmountDue.ReadOnly = true;
+            this.txtAmountDue.Size = new System.Drawing.Size(100, 22);
+            this.txtAmountDue.TabIndex = 9;
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(375, 469);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 10;
+            this.btnTotal.Text = "&Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(477, 469);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(577, 469);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 453);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(125, 17);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Samuel G Greelee";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(34, 494);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(46, 17);
+            this.lblDate.TabIndex = 13;
+            this.lblDate.Text = "label9";
+            // 
+            // frmCenterOfCars
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 520);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.txtAmountDue);
+            this.Controls.Add(this.txtTradeInAllowance);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.txtSalesTax);
+            this.Controls.Add(this.txtSubtotal);
+            this.Controls.Add(this.txtAAndFinish);
+            this.Controls.Add(this.txtSalePrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmCenterOfCars";
+            this.Text = "Center Of Cars";
+            this.Load += new System.EventHandler(this.frmCenterOfCars_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkGPSNavigation;
+        private System.Windows.Forms.CheckBox chkLeatherInterior;
+        private System.Windows.Forms.CheckBox chkStereoSystem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdoCustomizedDetailing;
+        private System.Windows.Forms.RadioButton rdoPearl;
+        private System.Windows.Forms.RadioButton rdoStandard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtSalePrice;
+        private System.Windows.Forms.TextBox txtAAndFinish;
+        private System.Windows.Forms.TextBox txtSubtotal;
+        private System.Windows.Forms.TextBox txtSalesTax;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.TextBox txtTradeInAllowance;
+        private System.Windows.Forms.TextBox txtAmountDue;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblDate;
+    }
+}
+
---
+++ VehiclePurchase/VehiclePurchase/Form1.cs
@@ -0,0 +1,296 @@
+﻿/*
+ * Author: Samuel Greenlee
+ * Project: Assignment Two
+ * Date  : January 30, 2020
+ * Desc  : Calculates the price of a car
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VehiclePurchase
+{
+    public partial class frmCenterOfCars : Form
+    {
+        public frmCenterOfCars()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCenterOfCars_Load(object sender, EventArgs e)
+        {
+            DateTime currentDate = DateTime.Today;
+            lblDate.Text = currentDate.ToLongDateString(); 
+            Reset();
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            // Declaire variables
+            decimal salePrice;
+            decimal accessoriesAmount;
+            decimal finishAmount;
+            decimal totalFinishAccessoriesAmount;
+            decimal subtotal;
+            decimal salesTax;
+            decimal total;
+            decimal tradeInAllowance;
+            decimal rangeCheck;
+            decimal amountCheck;
+
+            if (DataValid() == true) // Initiates data validation
+            {
+                // Get sale price
+                salePrice = GetSalePrice(out salePrice);
+
+                // Calculate the accessories and finish
+                accessoriesAmount = CalculateAccessories();
+
+                finishAmount = CalculateFinishAmount();
+
+                totalFinishAccessoriesAmount = CalculateTotalFinishAccessoriesAmount(out totalFinishAccessoriesAmount, accessoriesAmount, finishAmount);
+
+                // Calculate subtotal
+                subtotal = CalculateSubtotal(out subtotal, salePrice, totalFinishAccessoriesAmount);
+
+                // Calculate sales tax (8%)
+                salesTax = CalculateSalesTax(out salesTax, subtotal);
+
+                // Calculate total
+                total = CalculateTotal(out total, subtotal, salesTax);
+
+                // Get the trade-in allowance
+                tradeInAllowance = GetTradeInAllowance(out tradeInAllowance);
+
+                // Bool the trade-in allowance
+                TradeInAllowanceConfiguration(tradeInAllowance, total);
+
+                // Data validation check on sale price
+                rangeCheck = Convert.ToDecimal(txtSalePrice.Text);
+
+                // Data validation check on trade-in allowance
+                amountCheck = Convert.ToDecimal(txtTradeInAllowance.Text);
+
+                // This method displays the results needed in the read-only textboxes EXCEPT AMOUNT DUE
+                Display(totalFinishAccessoriesAmount, subtotal, salesTax, total);
+            }
+
+        }
+
+        private decimal GetSalePrice(out decimal salePrice)
+        {
+            salePrice = Convert.ToDecimal(txtSalePrice.Text);
+
+            return salePrice;
+        }
+
+
+        private decimal CalculateAccessories()
+        {
+            decimal price = 0.00m;
+
+            if (chkStereoSystem.Checked)
+            {
+                price += 425.76m;
+            }
+
+            if (chkLeatherInterior.Checked)
+            {
+                price += 987.41m;
+            }
+
+            if (chkGPSNavigation.Checked)
+            {
+                price += 1741.23m;
+            }
+
+            return price;
+        }
+
+        private decimal CalculateFinishAmount()
+        {
+            decimal price = 0.00m;
+
+            if (rdoStandard.Checked == true)
+            {
+                price = 0.00m;
+            }
+
+            if (rdoPearl.Checked == true)
+            {
+                price = 345.72m;
+            }
+
+            if (rdoCustomizedDetailing.Checked == true)
+            {
+                price = 599.99m;
+            }
+
+            return price;
+        }
+
+        private decimal CalculateTotalFinishAccessoriesAmount(out decimal totalFinishAccessoriesAmount, decimal accessoriesAmount, decimal finishAmount)
+        {
+            totalFinishAccessoriesAmount = accessoriesAmount + finishAmount;
+
+            return totalFinishAccessoriesAmount;
+
+        }
+
+        private decimal CalculateSubtotal(out decimal subtotal, decimal salePrice, decimal totalFinishAccessoriesAmount)
+        {
+            subtotal = salePrice + totalFinishAccessoriesAmount;
+
+            return subtotal;
+        }
+
+        private decimal CalculateSalesTax(out decimal salesTax, decimal subtotal)
+        {
+            salesTax = subtotal * 0.08m;
+
+            return salesTax;
+        }
+
+        private decimal CalculateTotal(out decimal total, decimal subtotal, decimal salesTax)
+        {
+            total = subtotal + salesTax;
+
+            return total;
+        }
+
+        private decimal GetTradeInAllowance(out decimal tradeInAllowance)
+        {
+            tradeInAllowance = Convert.ToDecimal(txtTradeInAllowance.Text);
+
+            return tradeInAllowance;
+        }
+
+        private bool TradeInAllowanceConfiguration(decimal tradeInAllowance, decimal total)
+        {
+            if (tradeInAllowance > 0)
+            {
+                total -= tradeInAllowance;
+
+                // This will display the amount due in currency, if the trade in allowance is greater than zero
+
+                txtAmountDue.Text = (total.ToString("f2")); // This rounds it to two decimal places
+
+                txtAmountDue.Text = total.ToString("c"); // This displays it in currancy 
+
+                return true;
+            }
+            return false;
+        }
+
+        // This is the methods containing data validation
+
+        private bool DataValid()
+        {
+            return
+                IsWhithInRange(txtSalePrice, "Sale price", 500, 30000) &&
+                CheckOnAmount(txtTradeInAllowance, "Trade-in allowance", txtSalePrice);
+        }
+
+        private bool IsWhithInRange(TextBox textBox, string name, decimal min, decimal max)
+        {
+            decimal number = Convert.ToDecimal(textBox.Text);
+            if (number < min || number > max) // || means or && means a conditional and
+            {
+                MessageBox.Show(name + " must be between " + min + " and " + max);
+                textBox.Focus();
+                textBox.SelectAll();
+                return false;
+
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        private bool CheckOnAmount(TextBox txtTradeInAllowance, string name, TextBox txtSalePrice)
+        {
+            decimal allowance = Convert.ToDecimal(txtTradeInAllowance.Text);
+            decimal priceOfSale = Convert.ToDecimal(txtSalePrice.Text);
+            if (allowance > priceOfSale)
+            {
+                MessageBox.Show(name + " must be less than the sale price");
+                txtTradeInAllowance.Focus();
+                txtTradeInAllowance.SelectAll();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        // This method displays all the data in the read-only textboxes
+
+        public void Display(decimal totalFinishAccessoriesAmount, decimal subtotal, decimal salesTax, decimal total)
+        {
+            // Displays accessories and finish               
+            txtAAndFinish.Text = (totalFinishAccessoriesAmount.ToString("f2")); // This rounds the to  two decimal places
+
+            // Displays subtotal 
+            txtSubtotal.Text = (subtotal.ToString("f2")); // This rounds the to two decimal places
+
+            // Displays sales tax (8%)
+            txtSalesTax.Text = (salesTax.ToString("f2")); // This rounds the to two decimal places
+
+            // Displays total
+            txtTotal.Text = (total.ToString("f2")); // This rounds the to two decimal places
+
+            txtTotal.Text = (total.ToString("c")); // By codeing it this way, it displays as currency
+        }
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            // Checked boxes
+
+            chkStereoSystem.Checked = false;
+            chkLeatherInterior.Checked = false;
+            chkGPSNavigation.Checked = false;
+
+            // Radio buttons
+
+            rdoStandard.Checked = true;
+            rdoPearl.Checked = false;
+            rdoCustomizedDetailing.Checked = false;
+
+            // Data in textboxes
+
+            txtSalePrice.Clear();
+            txtAAndFinish.Clear();
+            txtSubtotal.Clear();
+            txtSalesTax.Clear();
+            txtTotal.Clear();
+            txtTradeInAllowance.Clear();
+            txtAmountDue.Clear();
+
+            // Returns focus to the sale price textbox
+
+            txtSalePrice.Focus();
+        }
+
+        // Exit Button
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
